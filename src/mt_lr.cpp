2c9283e657f0d9b92a61635b823a715aba07ad57
Heather Goldsby <heather.goldsby@gmail.com>
2017-03-27 12:39:47 -0400

    mt_lr
    limited resources

---
+++ src/mt_lr.cpp
@@ -0,0 +1,209 @@
+#include <ea/digital_evolution.h>
+#include <ea/cmdline_interface.h>
+#include <ea/subpopulation_founder.h>
+#include <ea/line_of_descent.h>
+#include <ea/analysis/archive.h>
+//#include <ea/generational_models/periodic_competition.h>
+//#include <ea/generational_models/moran_process.h>
+//#include <ea/selection/rank.h>
+//#include <ea/datafiles/fitness.h>
+//#include <ea/digital_evolution/extra_instruction_sets/matrix.h>
+
+
+//#include "evolved_striped_ancestor2.h"
+//#include "multibirth_not_nand_prop_ancestor.h"
+
+//#include "subpopulation_propagule_split.h"
+
+
+
+#include "mt_propagule_orig.h"
+#include "multi_birth_selfrep_not_remote_ancestor.h"
+
+
+
+
+
+using namespace ealib;
+
+//#include "movie_evo_plane.h"
+
+
+//! Configuration object for an EA.
+struct lifecycle : public default_lifecycle {
+    
+    //! Called as the final step of EA construction (must not depend on configuration parameters)
+    template <typename EA>
+    void after_initialization(EA& ea) {
+        if(ea.isa().size()) {
+            return;
+        }
+        
+        using namespace instructions;
+        append_isa<nop_a>(0,ea);
+        append_isa<nop_b>(0,ea);
+        append_isa<nop_c>(0,ea);
+        append_isa<nop_x>(ea);
+        append_isa<mov_head>(ea);
+        append_isa<if_label>(ea);
+        append_isa<h_search>(ea);
+        append_isa<nand>(ea);
+        append_isa<push>(ea);
+        append_isa<pop>(ea);
+        append_isa<swap>(ea);
+        append_isa<inc>(ea);
+        append_isa<dec>(ea);
+        append_isa<tx_msg>(ea);
+        append_isa<rx_msg>(ea);
+        append_isa<bc_msg>(ea);
+        append_isa<rotate>(ea);
+        append_isa<rotate_cw>(ea);
+        append_isa<rotate_ccw>(ea);
+        append_isa<if_less>(ea);
+        append_isa<h_alloc>(ea);
+        append_isa<h_copy>(ea);
+        append_isa<h_divide_soft_parent_reset>(ea);
+        append_isa<input>(ea);
+        append_isa<output>(ea);
+        append_isa<donate_res_to_group>(ea);
+        append_isa<if_equal>(ea);
+        append_isa<if_not_equal>(ea);
+        append_isa<jump_head>(ea);
+        append_isa<is_neighbor>(ea);
+        append_isa<h_divide_remote>(ea);
+        append_isa<h_alt_divide>(ea);
+        
+        add_event<task_resource_consumption>(ea);
+        add_event<task_switching_cost>(ea);
+        
+        add_event<ts_birth_event>(ea);
+        
+        typedef typename EA::task_library_type::task_ptr_type task_ptr_type;
+        typedef typename EA::resource_ptr_type resource_ptr_type;
+        
+        task_ptr_type task_not = make_task<tasks::task_not,catalysts::additive<0> >("not", ea);
+        task_ptr_type task_nand = make_task<tasks::task_nand,catalysts::additive<0> >("nand", ea);
+        //        task_ptr_type task_and = make_task<tasks::task_and,catalysts::additive<0> >("and", ea);
+        //        task_ptr_type task_ornot = make_task<tasks::task_ornot,catalysts::additive<0> >("ornot", ea);
+        //        task_ptr_type task_or = make_task<tasks::task_or,catalysts::additive<0> >("or", ea);
+        //        task_ptr_type task_andnot = make_task<tasks::task_andnot,catalysts::additive<0> >("andnot", ea);
+        //        task_ptr_type task_nor = make_task<tasks::task_nor,catalysts::additive<0> >("nor", ea);
+        //        task_ptr_type task_xor = make_task<tasks::task_xor,catalysts::additive<0> >("xor", ea);
+        //        task_ptr_type task_equals = make_task<tasks::task_equals,catalysts::additive<0> >("equals", ea);
+        
+        //resource_ptr_type resA = make_resource("resA", ea);
+        //resource_ptr_type resB = make_resource("resB", ea);
+        
+        resource_ptr_type resA = make_resource("resA", 100.0, 1.0, 0.01, 0.05, ea);
+        resource_ptr_type resB = make_resource("resB", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resC = make_resource("resC", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resD = make_resource("resD", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resE = make_resource("resE", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resF = make_resource("resF", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resG = make_resource("resG", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resH = make_resource("resH", 100.0, 1.0, 0.01, 0.05, ea);
+        //        resource_ptr_type resI = make_resource("resI", 100.0, 1.0, 0.01, 0.05, ea);
+        
+        task_not->consumes(resA);
+        task_nand->consumes(resB);
+        //        task_and->consumes(resC);
+        //        task_ornot->consumes(resD);
+        //        task_or->consumes(resE);
+        //        task_andnot->consumes(resF);
+        //        task_nor->consumes(resG);
+        //        task_xor->consumes(resH);
+        //        task_equals->consumes(resI);
+        
+        
+    }
+    
+};
+
+template <typename T>
+struct subpop_trait : subpopulation_founder_trait<T>, fitness_trait<T> {
+    typedef subpopulation_founder_trait<T> parent1_type;
+    typedef fitness_trait<T> parent2_type;
+    
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version) {
+        ar & boost::serialization::make_nvp("subpopulation_founder_trait", boost::serialization::base_object<parent1_type>(*this));
+        ar & boost::serialization::make_nvp("fitness_trait", boost::serialization::base_object<parent2_type>(*this));
+    }
+};
+
+
+
+
+typedef digital_evolution
+< lifecycle
+, recombination::asexual
+, round_robin
+, multibirth_selfrep_not_remote_unfixed_ancestor
+, empty_facing_neighbor
+, dont_stop
+, generate_single_ancestor
+> sea_type;
+
+
+typedef metapopulation
+< sea_type
+, quiet_nan
+, mutation::operators::no_mutation
+, quiet_nan
+, generational_models::isolated_subpopulations
+, ancestors::default_subpopulation
+, dont_stop
+, fill_metapopulation
+, default_lifecycle
+, subpop_trait
+> mea_type;
+
+
+
+
+/*!
+ */
+template <typename EA>
+class cli : public cmdline_interface<EA> {
+public:
+    virtual void gather_options() {
+        add_option<SPATIAL_X>(this);
+        add_option<SPATIAL_Y>(this);
+        add_option<METAPOPULATION_SIZE>(this);
+        add_option<POPULATION_SIZE>(this);
+        add_option<REPRESENTATION_SIZE>(this);
+        add_option<SCHEDULER_TIME_SLICE>(this);
+        add_option<SCHEDULER_RESOURCE_SLICE>(this);
+        add_option<MUTATION_PER_SITE_P>(this);
+        add_option<MUTATION_INSERTION_P>(this);
+        add_option<MUTATION_DELETION_P>(this);
+        add_option<RUN_UPDATES>(this);
+        add_option<RUN_EPOCHS>(this);
+        add_option<RNG_SEED>(this);
+        add_option<RECORDING_PERIOD>(this);
+        add_option<MUTATION_UNIFORM_INT_MIN>(this);
+        add_option<MUTATION_UNIFORM_INT_MAX>(this);
+        
+        add_option<ANALYSIS_INPUT>(this);
+        
+        
+        // ts specific options
+        add_option<TASK_SWITCHING_COST>(this);
+        add_option<GERM_MUTATION_PER_SITE_P>(this);
+        add_option<GROUP_REP_THRESHOLD>(this);
+        
+        
+    }
+    
+    virtual void gather_tools() {
+        
+        
+    }
+    
+    virtual void gather_events(EA& ea) {
+        add_event<mt_propagule>(ea);
+        add_event<task_performed_tracking>(ea);
+        
+    }
+};
+LIBEA_CMDLINE_INSTANCE(mea_type, cli);

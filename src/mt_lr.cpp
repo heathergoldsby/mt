ab9e34c9441cc41b5d4d7de3c9952bd40812eedc
Heather Goldsby <heather.goldsby@gmail.com>
2017-04-13 13:37:27 -0400

    .

--- src/mt_lr.cpp
+++ src/mt_lr.cpp
@@ -192,6 +192,7 @@ public:
         add_option<TASK_SWITCHING_COST>(this);
         add_option<GERM_MUTATION_PER_SITE_P>(this);
         add_option<GROUP_REP_THRESHOLD>(this);
+        add_option<IND_REP_THRESHOLD>(this);
         
         
     }

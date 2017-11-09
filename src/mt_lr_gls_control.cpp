1bb332bef099633cd4950975712fa30174b25895
Heather Goldsby <heather.goldsby@gmail.com>
2017-11-09 09:36:29 -0500

    Added mt gls control

--- src/mt_lr_gls_control.cpp
+++ src/mt_lr_gls_control.cpp
@@ -84,7 +84,7 @@ struct lifecycle : public default_lifecycle {
         add_event<task_switching_cost>(ea);
         
         add_event<ts_birth_event>(ea);
-        add_event<task_mutagenesis>(ea);
+        add_event<task_mutagenesis_control>(ea);
         add_event<gs_inherit_event>(ea);
         
         typedef typename EA::task_library_type::task_ptr_type task_ptr_type;
@@ -149,7 +149,7 @@ struct lifecycle : public default_lifecycle {
         task_nor->consumes(resG);
         task_xor->consumes(resH);
         task_equals->consumes(resI);
-
+        
         
         
     }
@@ -247,7 +247,7 @@ public:
         add_option<RES_FRACTION_CONSUMED>(this);
         add_option<COST_RAMP>(this);
         add_option<COST_START_UPDATE>(this);
-
+        
         
         
     }

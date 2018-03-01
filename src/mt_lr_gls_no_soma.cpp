09da7cf3a09b933aa7a166d8a4090a67f46d3035
Heather Goldsby <heather.goldsby@gmail.com>
2018-03-01 14:36:21 -0500

    .

--- src/mt_lr_gls_no_soma.cpp
+++ src/mt_lr_gls_no_soma.cpp
@@ -84,6 +84,8 @@ struct lifecycle : public default_lifecycle {
         
         add_event<task_resource_consumption>(ea);
         add_event<task_switching_cost>(ea);
+        add_event<task_profile_tracking>(ea);
+
         
         add_event<ts_birth_event>(ea);
         add_event<task_mutagenesis>(ea);

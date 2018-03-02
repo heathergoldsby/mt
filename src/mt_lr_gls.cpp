52a012e550e154f7b17f863fb8baedb5a23abb94
Heather Goldsby <heather.goldsby@gmail.com>
2018-03-01 21:13:43 -0500

    .

--- src/mt_lr_gls.cpp
+++ src/mt_lr_gls.cpp
@@ -273,7 +273,7 @@ public:
         add_event<mt_gls_propagule>(ea);
         //add_event<datafiles::mrca_lineage>(ea);
         add_event<subpopulation_founder_event>(ea);
-        //add_event<task_performed_tracking>(ea);
+        add_event<task_performed_tracking>(ea);
         //add_event<task_switch_tracking>(ea);
         //add_event<dol_tracking>(ea);
         

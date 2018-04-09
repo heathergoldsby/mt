9d5d4aae3b7b3846fa3f93695340613f36caeec8
Heather Goldsby <heather.goldsby@gmail.com>
2018-04-09 10:47:24 -0400

    .

--- src/mt_lr_gls.cpp
+++ src/mt_lr_gls.cpp
@@ -276,6 +276,7 @@ public:
         add_tool<ealib::analysis::merge_archives>(this);
         add_tool<ealib::analysis::archive_population>(this);
         add_tool<ealib::analysis::lod_archive_reversion>(this);
+        add_tool<ealib::analysis::archive_dominant>(this);
 
     }
     

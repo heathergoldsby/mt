8c7e53185761d6ccb878408b941c6ccd7ff185c5
Heather Goldsby <heather.goldsby@gmail.com>
2019-07-25 21:56:48 -0400

    bug fix.

--- src/mt_lr_gls_control2.cpp
+++ src/mt_lr_gls_control2.cpp
@@ -176,7 +176,7 @@ typedef digital_evolution
 < lifecycle
 , recombination::asexual
 , round_robin
-, multibirth_selfrep_not_remote_ancestor
+, multibirth_selfrep_not_remote_ancestor_unfixed
 , empty_facing_neighbor
 , dont_stop
 , generate_single_ancestor

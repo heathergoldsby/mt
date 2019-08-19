f4e1f20d24f166fa6aa2c7586e489ff02faad220
Heather Goldsby <heather.goldsby@gmail.com>
2019-08-18 20:18:31 -0400

    Added new lod fitness tracking.

---
+++ src/lod_knockouts_fitness.h
@@ -0,0 +1,13 @@
+//
+//  lod_knockouts_fitness.h
+//  mt
+//
+//  Created by Heather Goldsby on 8/17/19.
+//  Copyright © 2019 Michigan State University. All rights reserved.
+//
+
+#ifndef lod_knockouts_fitness_h
+#define lod_knockouts_fitness_h
+
+
+#endif /* lod_knockouts_fitness_h */
